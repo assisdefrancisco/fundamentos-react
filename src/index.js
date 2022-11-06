53daf9493c5f5ddd94ec9004e4e0987b8fc46d49
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 12:17:05 -0300

    Usando o React.Fragment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM  from 'react-dom'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div>
@@ -11,6 +12,7 @@ ReactDOM.render(
             titulo="Situacao do Aluno" 
             aluno="Pedro" 
             nota={9.3} />
+        <Fragmento />
     </div>, 
     document.getElementById('root')
 )
