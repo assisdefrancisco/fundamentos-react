0847f04bc04cadf94a6517a4daa0818c1ad4d66c
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 16:45:39 -0300

    Criado Componente Card, e ajeitando o layout do projeto

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from './components/basicos/layout/Card'
 
 export default _ =>
     <div id="app">
         <h1>Fundamentos React 2</h1>
-        <Aleatorio min={1} max={10} />
-        <Fragmento />
-        <ComParametro
-            titulo="Situacao do Aluno"
-            aluno="Pedro"
-            nota={9.3} />
-        <Primeiro></Primeiro>
+        <Card titulo="#04 - Desafio Aleatório">
+            <Aleatorio min={1} max={60} />
+        </Card>
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+        <Card titulo="#02 - Com Parâmetro">
+            <ComParametro
+                titulo="Situacao do Aluno"
+                aluno="Pedro"
+                nota={9.3} />
+        </Card>
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
     </div>
