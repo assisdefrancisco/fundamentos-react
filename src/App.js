ca055564001cfc10968368d3ccafd911a6a5b6dd
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 12:28:00 -0300

    Criado componente App, com array function

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+export default (props) => {
+    return (
+        <div id="app">
+            <h1>Fundamentos React 2</h1>
+            <Fragmento />
+            <ComParametro
+                titulo="Situacao do Aluno"
+                aluno="Pedro"
+                nota={9.3} />
+            <Primeiro></Primeiro>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM  from 'react-dom'
+import App from './App'
 import './index.css'
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+
 
 ReactDOM.render(
-    <div>
-        <Primeiro></Primeiro>
-        <ComParametro 
-            titulo="Situacao do Aluno" 
-            aluno="Pedro" 
-            nota={9.3} />
-        <Fragmento />
-    </div>, 
+     <App />,
     document.getElementById('root')
 )
