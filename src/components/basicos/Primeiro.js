8c7360b05944adf20a6efe328d7bc24e37f9de48
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 02:08:11 -0300

    Alterando componente para usar jsx

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function Primeiro() {
-    return <h1>Primeiro Componente</h1>
+    const msg = "Seja bem vindo(a)!"
+
+
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
