269b1f431ef4b98274d57dba5da3220da9f91865
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 12:29:57 -0300

    Simplificando a array function

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 
-export default _ => {
-    return (
-        <div id="app">
-            <h1>Fundamentos React 2</h1>
-            <Fragmento />
-            <ComParametro
-                titulo="Situacao do Aluno"
-                aluno="Pedro"
-                nota={9.3} />
-            <Primeiro></Primeiro>
-        </div>
-    )
-}
+export default _ =>
+    <div id="app">
+        <h1>Fundamentos React 2</h1>
+        <Fragmento />
+        <ComParametro
+            titulo="Situacao do Aluno"
+            aluno="Pedro"
+            nota={9.3} />
+        <Primeiro></Primeiro>
+    </div>
