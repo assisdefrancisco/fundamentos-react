c7c35c57f43a005ec6c1a6486c7c6b30efcc2a5d
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 11:55:03 -0300

    Passagem de parametro no componente

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM  from 'react-dom'
 import './index.css'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
-        <Primeiro></Primeiro>        
+        <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Segundo Componente" 
+            subtitulo="Componente com parametro" />
     </div>, 
     document.getElementById('root')
 )
