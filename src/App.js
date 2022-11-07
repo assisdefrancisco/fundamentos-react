532344f041760fee779fe595a1ab8b7723a0435c
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 22:22:19 -0300

    Alterando passagem de propriedade do Componente Pai para o Filho, apenas com um componente filho

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/basicos/layout/Card'
 import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 
 export default _ =>
     <div className="app">
         <h1>Fundamentos React 2</h1>
         <div className="cards">
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-                <Familia sobrenome="Ferreira" />
+                <Familia sobrenome="Ferreira">
+                    <FamiliaMembro nome="Pedro" />
+                    {/* <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" /> */}
+                </Familia>
             </Card>
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60} />
--- src/components/basicos/Familia.jsx
+++ src/components/basicos/Familia.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import FamiliaMembro from './FamiliaMembro'
+import React, { cloneElement } from 'react'
 
 export default props => {
     return (
         <div>
-            <FamiliaMembro nome="Pedro" sobrenome={props.sobrenome} />
-            <FamiliaMembro nome="Ana" {...props} /> {/* Operador spreed passa tudo de atributo para o componente filho */}            
-            <FamiliaMembro nome="Gustavo"  sobrenome="Silva" />
+            {cloneElement(props.children, props)}
         </div>
     )
 }
