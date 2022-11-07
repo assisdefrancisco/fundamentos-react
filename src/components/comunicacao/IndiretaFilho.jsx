7634875d3db39122ab6517e84dd6f2abfdce138e
francisco <franciscoassisdeveloper@gmail.com>
2022-11-07 02:19:51 -0300

    Resolvendo o problema anterior usando estado dentro do componente, qualquer mudança nos dados é refletido na interface gráfica

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 export default props => {
     const callback = props.quandoClicar;
 
+    const min = 50
+    const max = 70
+
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gerarNerd = () => Math.random() > 0.5
+
     return (
         <div>
             <div>Filho</div>
             <button onClick={
-                _ => callback('João', 53, true)
+                _ => callback('João', gerarIdade(), gerarNerd())
             }>Fornecer INformações</button>
         </div>
     )
