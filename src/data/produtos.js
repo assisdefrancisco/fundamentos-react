f0e63aaa480996612a803b375f6d7724ac04369a
francisco <franciscoassisdeveloper@gmail.com>
2022-11-06 23:32:23 -0300

    Uma forma de exportar dados

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -1,5 +1,13 @@
-export default [
+// export default [
+//     { id: 1, nome: 'Arroz', preco: 15.99 },
+//     { id: 2, nome: 'Feijão', preco: 5.79 },
+//     { id: 3, nome: 'Óleo de Cozinha', preco: 4.99 },
+// ]
+
+const produtos = [
     { id: 1, nome: 'Arroz', preco: 15.99 },
     { id: 2, nome: 'Feijão', preco: 5.79 },
     { id: 3, nome: 'Óleo de Cozinha', preco: 4.99 },
 ]
+
+export default produtos;
